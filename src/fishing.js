57d5dd7f12e06e14ea97a1ebcee8d3c66628bf41
shanhong cheng <shanhong.cheng@utah.edu>
2017-06-12 15:28:37 -0600

    waterbody

--- src/fishing.js
+++ src/fishing.js
@@ -7,3 +7,44 @@ import cheerio from 'cheerio';
 import request from 'request';
 
 dotenv.config();
+
+const UTAH_FISHING_URL = "https://wildlife.utah.gov/hotspots/";
+
+function findTextAndReturnRemainder(target, variable) {
+  let index = target.search(variable);
+  if(index == -1) {
+    return null;
+  }
+  let chopFront = target.substring(index + variable.length, target.length);
+  let result = chopFront.substring(0, chopFront.search(";"));
+  return result;
+}
+
+function getWaterBody(text) {
+  if (!text) {
+    return;
+  }
+  let findAndClean = findTextAndReturnRemainder(text, "var waterbody =")
+  if (findAndClean) {
+    var result = eval(findAndClean);
+
+    result.forEach((waterbody) => {
+      console.log(waterbody)
+    })
+  }
+}
+
+function getHotSpots() {
+  request(UTAH_FISHING_URL, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      let $ = cheerio.load(html);
+
+      $('script').each((index, element) => {
+        getWaterBody($(element).html());
+      });
+
+    }
+  })
+}
+
+getHotSpots();
